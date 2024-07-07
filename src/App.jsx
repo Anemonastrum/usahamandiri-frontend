6b998d84de921311a40c0fbe082a0f735e608a1d
anemonastrum <encat.cat@gmail.com>
2024-07-07 22:10:56 +0700

    add template for register and login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/landing.jsx";
+import Login from './pages/login.jsx';
+import Register from './pages/register.jsx';
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
+import { UserContextProvider } from './context/userContext.jsx';
+import { Toaster } from 'react-hot-toast'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:8000/api/'
+axios.defaults.withCredentials = true
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
@@ -11,11 +19,16 @@ export const scroll = new SmoothScroll('a[href*="#"]', {
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-      </Routes>
-    </Router>
+    <UserContextProvider>
+      <Toaster position='top-right' toastOptions={{duration: 2000}} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </Router>
+    </UserContextProvider>
   );
 };
 
